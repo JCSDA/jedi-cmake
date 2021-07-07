0fd301522fc0502666712727320d6c504adb10c3
mmiesch <miesch@ucar.edu>
2021-07-07 23:40:55 +0000

    Must explicitly specify mpirun as well

--- cmake/Toolchains/jcsda-AWS-NVHPC.cmake
+++ cmake/Toolchains/jcsda-AWS-NVHPC.cmake
@@ -5,6 +5,13 @@
 set( ECBUILD_CXX_FLAGS      "-Wc,--pending_instantiations=128" CACHE STRING "NVHPC base CXX compiler flags" FORCE )
 
 ####################################################################
+# MPI
+####################################################################
+
+set( MPIEXEC_EXECUTABLE /opt/nvidia/hpc_sdk/Linux_x86_64/21.5/comm_libs/mpi/bin/mpirun CACHE STRING "Use nvidia SDK" FORCE )
+set( MPIEXEC_NUMPROC_FLAG "-np" CACHE STRING "NUMPROC flag for mpirun" FORCE)
+
+####################################################################
 # Options
 ####################################################################
 
